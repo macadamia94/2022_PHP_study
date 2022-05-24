6c17e1509391241e1778593cf486d7fe3dfd6fef
macadamia94 <thswlgus15@hanmail.net>
2022-05-24 16:51:12 +0900

    add 'bootstrap'

---
+++ bootstrap/figure.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <figure>
+        <img src="https://i.picsum.photos/id/744/500/300.jpg?hmac=ROyuqsr6VdtIxI9SR5dDriDaMeJwApziqwMK5dHXGxg" alt="bridge">
+        <figcaption>A caption for the above image.</figcaption>
+    </figure>
+</body>
+
+</html>
