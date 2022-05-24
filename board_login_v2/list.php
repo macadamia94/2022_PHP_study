36ec6b94cf8188874f997165bf0ae41a9a05d1c6
macadamia94 <thswlgus15@hanmail.net>
2022-05-24 13:14:44 +0900

    add 'bootstrap'

--- board_login_v2/list.php
+++ board_login_v2/list.php
@@ -1,36 +1,37 @@
-<?php   
-    include_once "db/db_board.php";
+<?php
+include_once "db/db_board.php";
 
-    session_start();
-    $nm = "";
-    if(isset($_SESSION["login_user"])) {
-        $login_user = $_SESSION["login_user"];
-        $nm = $login_user["nm"];
-    }
-    $page= 1;
-    if(isset($_GET["page"])) {
-        $page= intval($_GET["page"]);
-    }
-    // intval : 정수형으로 변형
-    $search_txt= "";
-    if(isset($_GET["search_txt"])) {
-        $search_txt= $_GET["search_txt"];
-    }
+session_start();
+$nm = "";
+if (isset($_SESSION["login_user"])) {
+    $login_user = $_SESSION["login_user"];
+    $nm = $login_user["nm"];
+}
+$page = 1;
+if (isset($_GET["page"])) {
+    $page = intval($_GET["page"]);
+}
+// intval : 강제로 정수형으로 변형
+$search_txt = "";
+if (isset($_GET["search_txt"])) {
+    $search_txt = $_GET["search_txt"];
+}
 
-    $row_count= 10;
-    // 페이지당 보여지는 갯수
+$row_count = 10;
+// 페이지당 보여지는 갯수
 
-    $param= [
-        "s_idx" => ($page - 1) * $row_count,
-        "row_count" => $row_count,
-        "search_txt" => $search_txt
-    ];
+$param = [
+    "s_idx" => ($page - 1) * $row_count,
+    "row_count" => $row_count,
+    "search_txt" => $search_txt
+];
 
-    $paging_count= sel_paging_count($param);
-    $list = sel_board_list($param);
+$paging_count = sel_paging_count($param);
+$list = sel_board_list($param);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -38,18 +39,19 @@
     <link rel="stylesheet" href="common.css">
     <title>리스트</title>
 </head>
+
 <body>
     <div id="container">
         <header>
-            <?=isset($_SESSION["login_user"]) ? "<div>". $nm . "님 환영합니다.</div>" : "" ?>
+            <?= isset($_SESSION["login_user"]) ? "<div>" . $nm . "님 환영합니다.</div>" : "" ?>
             <div>
                 <a href="list.php">리스트</a>
-                <?php if(isset($_SESSION["login_user"])) { ?>
+                <?php if (isset($_SESSION["login_user"])) { ?>
                     <a href="write.php">글쓰기</a>
                     <a href="logout.php">로그아웃</a>
                 <?php } else { ?>
                     <a href="login.php">로그인</a>
-                <?php } ?>                
+                <?php } ?>
             </div>
         </header>
         <main>
@@ -57,7 +59,7 @@
             <div>
                 <form action="list.php" method="get">
                     <div>
-                        <input type="search" name="search_txt" value="<?=$search_txt?>">
+                        <input type="search" name="search_txt" value="<?= $search_txt ?>">
                         <input type="submit" value="검색">
                     </div>
                 </form>
@@ -71,27 +73,29 @@
                         <th>등록일시</th>
                     </tr>
                 </thead>
-                <tbody>               
-                    <?php foreach($list as $item) { ?>
+                <tbody>
+                    <?php foreach ($list as $item) { ?>
                         <tr>
-                            <td><?=$item["i_board"]?></td>
-                            <td><a href="detail.php?i_board=<?=$item["i_board"]?>&page=<?=$page?><?=$search_txt !== "" ? "&search_txt=$search_txt" : "" ?>"><?=str_replace($search_txt, "<mark>{$search_txt}</mark>", $item["title"])?></a></td>
-                            <td><?=$item["nm"]?></td>
-                            <td><?=$item["created_at"]?></td>
+                            <td><?= $item["i_board"] ?></td>
+                            <td><a href="detail.php?i_board=<?= $item["i_board"] ?>&page=<?= $page ?><?= $search_txt === "" ? "" : "&search_txt={$search_txt}" ?>"><?= str_replace($search_txt, "<mark>{$search_txt}</mark>", $item["title"]) ?></a></td>
+                            <!-- 문자열변환 : str_replace(찾을값, 바꿀값, 원본) -->
+                            <td><?= $item["nm"] ?></td>
+                            <td><?= $item["created_at"] ?></td>
                         </tr>
                     <?php } ?>
                 </tbody>
             </table>
             <div>
                 <?php
-                for($i=1; $i<=$paging_count; $i++) { ?>
-                    <span class="<?=$i==$page ? "selected_page" : ""?>">
-                        <a href="list.php?page=<?=$i?><?=$search_txt !== "" ? "&search_txt=$search_txt" : "" ?>"><?=$i?></a>
-                                                    <!-- $search_txt !== "" ? "&search_txt=" . $search_txt" : "" -->
+                for ($i = 1; $i <= $paging_count; $i++) { ?>
+                    <span class="<?= $i == $page ? "selected_page" : "" ?>">
+                        <a href="list.php?page=<?= $i ?><?= $search_txt === "" ? "" : "&search_txt={$search_txt}" ?>"><?= $i ?></a>
+                        <!-- $search_txt !== "" ? "&search_txt=" . $search_txt" : "" -->
                     </span>
                 <?php } ?>
             </div>
         </main>
     </div>
 </body>
+
 </html>
