db0bbbb902b272da588c1808bf6f9a8e30317ddf
macadamia94 <thswlgus15@hanmail.net>
2022-06-07 23:43:18 +0900

    .

--- ch11/cookie2.php
+++ ch11/cookie2.php
@@ -1,12 +1,20 @@
 <?php
-    $cookie_key = 'aaa';
-
+    $cookie_key = "aaa";
+   
     echo "Country : ", $_COOKIE[$cookie_key], "<br>";
 
-    $_COOKIE[$cookie_key] = "UK"; // 메모리상에 쿠기는 변경하지 않는 것이 좋다
-    echo "Country : ", $_COOKIE[$cookie_key], "<br>";
+    setcookie($cookie_key, "한국");
+    
 
+    $country = $_COOKIE[$cookie_key];
+
+    $country = 'UK';
+
+    echo "Country : ",  $country, "<br>";
+    
     /*
+    $_COOKIE[$cookie_key] = "UK"; // 메모리상에 쿠기는 변경하지 않는 것이 좋다
+
     // 쿠키를 바꿀경우 변수로 바꾸는 것이 좋음
     $country = $_COOKIE[$cookie_key];
     $country ='UK';
